eb87cab8174bd83a388ab777fb9635c3968fcb10
mateuszdabski <mateuszdabski96@gmail.com>
2019-11-10 17:59:54 +0100

    Added detail view for single article via link

--- djangoblog/articles/views.py
+++ djangoblog/articles/views.py
@@ -4,3 +4,7 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
