5e9d02970b073f4d6d2e58b1719f26f105542d7e
mateuszdabski <mateuszdabski96@gmail.com>
2019-11-10 20:10:55 +0100

    Added requiring login to create article

--- djangoblog/accounts/views.py
+++ djangoblog/accounts/views.py
@@ -19,7 +19,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('articles:list')
+            return redirect('articles:create')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', { 'form':form })
