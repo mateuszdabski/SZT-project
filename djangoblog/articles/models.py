35aba82a0cce2fe6b48ecfc5f6d01b78704395a4
mateuszdabski <mateuszdabski96@gmail.com>
2019-11-12 21:16:48 +0100

    Minor fixes and changes

--- djangoblog/articles/models.py
+++ djangoblog/articles/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.jpg', blank=True)
